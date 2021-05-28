c09dec5fbf89594bfcdfb14933ceabf88c2d8284
vladimir281997 <vladimir.hudko@student.nhlstenden.com>
2021-05-28 13:37:07 +0200

    database

---
+++ Repositories/ClassModels/Database.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Dog_school.Repositories
+{
+    public class Database
+    {
+            protected string Table { get; set; }
+            private MySqlConnection _connection;
+            public string ConnectionString { get; set; }
+
+            private MySqlConnection GetConnection()
+            {
+                this._connection = new MySqlConnection("server=127.0.0.1;port=3306;database=hondenschool;uid=root;pwd=Test@1234!;");
+                this._connection.Open();
+                return this._connection;
+            }
+        
+    }
+}
