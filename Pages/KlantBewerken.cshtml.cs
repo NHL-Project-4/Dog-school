7d013b28299afd3a11a3e4a74495a4a4f489bb50
HyperPandax <39954733+HyperPandax@users.noreply.github.com>
2021-05-31 13:36:09 +0200

    klantbewerken pagina

---
+++ Pages/KlantBewerken.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dog_school.Pages
+{
+    public class KlantBewerken : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
